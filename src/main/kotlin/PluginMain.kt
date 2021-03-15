98d024ffa16b444cc4e9b76462039186aea9930b
Kreedzt <zhaozisong1@live.com>
2021-03-15 21:58:33 +0800

    :sparkles: Kreedzt - modify plugin template

--- src/main/kotlin/PluginMain.kt
+++ src/main/kotlin/PluginMain.kt
@@ -1,20 +1,27 @@
 package org.example.mirai.plugin
 
+import net.mamoe.mirai.console.command.CommandSender.Companion.toCommandSender
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
+import net.mamoe.mirai.event.events.FriendMessageEvent
+import net.mamoe.mirai.event.events.GroupMessageEvent
+import net.mamoe.mirai.event.events.MessageEvent
+import net.mamoe.mirai.event.globalEventChannel
+import net.mamoe.mirai.message.data.LightApp
+import net.mamoe.mirai.message.data.MessageContent
+import net.mamoe.mirai.message.data.content
 import net.mamoe.mirai.utils.info
 
 object PluginMain : KotlinPlugin(
     JvmPluginDescription(
-        id = "org.example.mirai-plugin",
-        name = "ExamplePlugin",
+        id = "org.example.mirai-parse-miniprogram-plugin",
+        name = "ParseMiniProgramPlugin",
         version = "0.1.0"
     ) {
-        author("作者名称或联系方式")
+        author("Kreedzt")
 
         info("""
-            这是一个测试插件, 
-            在这里描述插件的功能和用法等.
+            解析QQ小程序链接
         """.trimIndent())
 
         // author 和 info 可以删除.
@@ -22,5 +29,24 @@ object PluginMain : KotlinPlugin(
 ) {
     override fun onEnable() {
         logger.info { "Plugin loaded" }
+
+        this.globalEventChannel().subscribeAlways<MessageEvent>{ event ->
+            logger.info(event.toString())
+            logger.info(event.message.toString())
+            this.toCommandSender().sendMessage("Test Res")
+        }
+
+        this.globalEventChannel().subscribeAlways<FriendMessageEvent>{ event ->
+            logger.info("This is friendMessage: ${event.senderName}")
+            event.toCommandSender().sendMessage("朋友消息")
+            logger.info("消息内容: ${event.message.content}")
+            val la = LightApp(event.message.content)
+            logger.info("la: ${la}")
+        }
+
+        this.globalEventChannel().subscribeAlways<GroupMessageEvent> { event ->
+            logger.info("This is groupMessage: ${event.senderName}")
+            event.toCommandSender().sendMessage("群组消息")
+        }
     }
 }
